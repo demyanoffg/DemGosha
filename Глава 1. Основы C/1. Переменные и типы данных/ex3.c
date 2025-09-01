c8b2295067c2b49c5ff305627f1889bf00410db5
demyanoffg <demyanoffg@gmail.com>
2025-09-01 18:26:17 +0300

    Update ex3.c

--- Глава 1. Основы C/1. Переменные и типы данных/ex3.c
+++ Глава 1. Основы C/1. Переменные и типы данных/ex3.c
@@ -1,19 +1,11 @@
 /*Напишите программу, в которой определите четыре переменных типов char, int, float и double и инциализируйте их следующими значениями:
-
 char
-
 'F'
-
 int
-
 10
-
 float
-
 3.14
-
 double
-
 3.14159*/
 #include <stdio.h>
   
@@ -24,4 +16,5 @@ int main(void)
     float f = 3.14f;
     double d = 3.14159;
     return 0;
+
 }
